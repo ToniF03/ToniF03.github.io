f72ec1307f24185a4b76f1200c7eb1f9a33edae4
Toni Fey <ToniFey@protonmail.com>
2025-11-01 00:50:02 +0100

    Refactor app.js and index.html for improved structure and readability; update age calculation method

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ function linkToSection(element) {
     navigator.clipboard.writeText(window.location.pathname + '#' + elementId);
     window.location.href = window.location.pathname + '#' + elementId;
 }
+
+function calculateAge() {
+    document.getElementById('age').innerHTML = String(new Date(Date.now() - new Date(2003, 12, 10)).getUTCFullYear() - 1970);
+}
